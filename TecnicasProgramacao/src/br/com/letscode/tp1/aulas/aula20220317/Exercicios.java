00453d1d55fc5d055581a53f999de6d0cf53e0f4
Guilherme Gontijo <glgontijo@yahoo.com.br>
2022-03-23 10:13:22 -0300

    Aula 20220322

--- TecnicasProgramacao/src/br/com/letscode/tp1/aulas/aula20220317/Exercicios.java
+++ TecnicasProgramacao/src/br/com/letscode/tp1/aulas/aula20220317/Exercicios.java
@@ -36,8 +36,10 @@ public class Exercicios {
     public static class Exercicio04 {
         public static void main(String[] args) {
             // 4. Formatar uma data no seguinte padrão: "19:00:00, 17 de março de 2022 (fuso: -03:00)"
-            OffsetDateTime offsetDateTime = OffsetDateTime.of(2022, 3, 17, 19, 0, 0, 0, ZoneOffset.of("-3"));
-            System.out.println(offsetDateTime.format(DateTimeFormatter.ofPattern("HH:mm:ss', 'd 'de 'LLLL 'de 'yyyy '(fuso: 'xxx')'")));
+            OffsetDateTime offsetDateTime = OffsetDateTime
+                    .of(2022, 3, 17, 19, 0, 0, 0, ZoneOffset.of("-3"));
+            System.out.println(offsetDateTime.format(DateTimeFormatter
+                            .ofPattern("HH:mm:ss', 'd 'de 'LLLL 'de 'yyyy '(fuso: 'xxx')'")));
         }
     }
 }
